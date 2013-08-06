cf4db265d39ead9752b6059e937c9bef6269a611
Ilya Nemihin <nemilya@gmail.com>
2013-08-06 20:03:37 +0600

    try page

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-#require 'rubygems'
+require 'rubygems'
 require 'sinatra'
 require "sinatra/reloader" if File.exists?('c:/')
 
@@ -76,4 +76,27 @@ post '/update' do
   publish_to_wp(blog_domain, wp_post_id, post, wp_login, wp_pwd)
 
   redirect '/'
+end
+
+get '/try' do
+  haml :try
+end
+
+post '/try' do
+  cnt  = get_cnt_from_url(params[:url])
+  html = markdown_to_html(cnt)
+  # read more is '---'
+  html.gsub!(/<hr>/, '<!--more-->')
+
+  blog_domain = params[:blog]
+  wp_post_id  = params[:wp_id]
+  wp_login    = params[:login]
+  wp_pwd      = params[:password]
+
+  post = {}
+  # post['title'] = '...'
+  post['description'] = html
+  publish_to_wp(blog_domain, wp_post_id, post, wp_login, wp_pwd)
+
+  redirect '/try'
 end
