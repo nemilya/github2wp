7849813aa3ff5f968d75d1a55aa1362dcc98ce72
Ilya Nemihin <nemilya@gmail.com>
2013-08-07 14:30:56 +0600

    refactoring

--- app.rb
+++ app.rb
@@ -66,21 +66,14 @@ end
 
 post '/update' do
   url_item = @urls[params[:pos].to_i]
-  cnt  = get_cnt_from_url(url_item['url'])
-  html = markdown_to_html(cnt)
-  # read more is '---'
-  html.gsub!(/<hr>/, '<!--more-->')
 
-  
+  source_url  = url_item['url']
   blog_domain = @config['blog']
   wp_post_id  = url_item['wp_id']
   wp_login    = params[:login]
   wp_pwd      = params[:password]
 
-  post = {}
-  # post['title'] = '...'
-  post['description'] = html
-  publish_to_wp(blog_domain, wp_post_id, post, wp_login, wp_pwd)
+  publish_from_source_to_wordpress(source_url, blog_domain, wp_post_id, wp_login, wp_pwd)
 
   redirect '/'
 end
@@ -92,20 +85,13 @@ get '/try' do
 end
 
 post '/try' do
-  cnt  = get_cnt_from_url(params[:url])
-  html = markdown_to_html(cnt)
-  # read more is '---'
-  html.gsub!(/<hr>/, '<!--more-->')
-
+  source_url  = params[:url]
   blog_domain = params[:blog]
   wp_post_id  = params[:wp_id]
   wp_login    = params[:login]
   wp_pwd      = params[:password]
 
-  post = {}
-  # post['title'] = '...'
-  post['description'] = html
-  publish_to_wp(blog_domain, wp_post_id, post, wp_login, wp_pwd)
+  publish_from_source_to_wordpress(source_url, blog_domain, wp_post_id, wp_login, wp_pwd)
 
   redirect '/try'
 end
